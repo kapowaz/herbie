2668bbb2f6e6aa149654a496d53949aaf3f5c863
Ben Darlow <github@kapowaz.net>
2011-10-10 01:39:06 +0100

    Bumped version to 0.1.2 to remove redundant reference to erubis dependency

--- lib/herbie.rb
+++ lib/herbie.rb
@@ -1,8 +1,7 @@
-require 'erubis'
 require 'herbie/erb_helpers.rb'
 require 'herbie/generic_helpers.rb'
 require 'herbie/html_helpers.rb'
 
 module Herbie
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
 end
